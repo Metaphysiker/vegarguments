8d75c9d0ee709b0d4609e4f9cdf3c979ff8c81f2
Metaphysiker <s.raess@me.com>
2018-05-28 22:19:26 +0200

    dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# rake db:seed:dump MODELS=Argument,Question APPEND=true
+
 Language.create!([
   {name: "Deutsch", abbreviation: "de"},
   {name: "English", abbreviation: "en"}
